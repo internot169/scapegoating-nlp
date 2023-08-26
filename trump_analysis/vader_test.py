3452ee10c37eff1639da1081ec50f8db9357a1df
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-08-26 00:29:55 -0700

    Analyze Reuters Dataset

--- trump_analysis/vader_test.py
+++ trump_analysis/vader_test.py
@@ -2,8 +2,8 @@ from nltk import sent_tokenize
 import nssm
 import pandas
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # number of days to group for visualization
 GROUPING_SIZE = 30.4167
@@ -22,7 +22,7 @@ def sentiment(text):
 
     return mean
 
-def group_by_time_series(df):
+def group_by_time_series(df, nrows):
     for index, row in df.iterrows():
         df.loc[index, 'Time_Num'] = (float(row['Year']) * 365) + (float(row['Month']) * 30.4167) + float(row['Day'])
     
@@ -32,7 +32,16 @@ def group_by_time_series(df):
 
     max_lim = GROUPING_SIZE
 
-    cleaned_dict = {
+    a_dict = {
+
+    }
+    b_dict = {
+
+    }
+    c_dict = {
+
+    }
+    s_dict = {
 
     }
 
@@ -40,55 +49,96 @@ def group_by_time_series(df):
         if row['Time_Num'] >= max_lim:
             max_lim += GROUPING_SIZE
 
-        if str(max_lim) not in cleaned_dict:
-            cleaned_dict[str(max_lim)] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        key = str(max_lim)
 
+        if (key not in a_dict):
+            a_dict[key] = [0,0,0]
         if row['A_Sentiment_Final'] == -1:
-            cleaned_dict[str(max_lim)][0] += 1
+            a_dict[key][0] += 1
         elif row['A_Sentiment_Final'] == 0:
-            cleaned_dict[str(max_lim)][1] += 1
+            a_dict[key][1] += 1
         elif row['A_Sentiment_Final'] == 1:
-            cleaned_dict[str(max_lim)][2] += 1
+            a_dict[key][2] += 1
+        a_dict[key][0] = (a_dict[key][0] / nrows) * 100
+        a_dict[key][1] = (a_dict[key][1] / nrows) * 100
+        a_dict[key][2] = (a_dict[key][2] / nrows) * 100
         
+        if (key not in b_dict):
+            b_dict[key] = [0,0,0]
         if row['B_Sentiment_Final'] == -1:
-            cleaned_dict[str(max_lim)][3] += 1
+            b_dict[key][0] += 1
         elif row['B_Sentiment_Final'] == 0:
-            cleaned_dict[str(max_lim)][4] += 1
+            b_dict[key][1] += 1
         elif row['B_Sentiment_Final'] == 1:
-            cleaned_dict[str(max_lim)][5] += 1
-        
+            b_dict[key][2] += 1
+        b_dict[key][0] /= nrows
+        b_dict[key][1] /= nrows
+        b_dict[key][2] /= nrows
+
+        if (key not in c_dict):
+            c_dict[key] = [0,0,0]
         if row['C_Sentiment_Final'] == -1:
-            cleaned_dict[str(max_lim)][6] += 1
+            c_dict[key][0] += 1
         elif row['C_Sentiment_Final'] == 0:
-            cleaned_dict[str(max_lim)][7] += 1
+            c_dict[key][1] += 1
         elif row['C_Sentiment_Final'] == 1:
-            cleaned_dict[str(max_lim)][8] += 1
-        
+            c_dict[key][2] += 1
+        c_dict[key][0] /= nrows
+        c_dict[key][1] /= nrows
+        c_dict[key][2] /= nrows
+
+        if (key not in s_dict):
+            s_dict[key] = [0,0,0]
         if row['Sentiment'] <= -0.05:
-            cleaned_dict[str(max_lim)][6] += 1
+            s_dict[key][0] += 1
         elif row['Sentiment'] > -0.05 and row['Sentiment'] < 0.05:
-            cleaned_dict[str(max_lim)][7] += 1
+            s_dict[key][1] += 1
         elif row['Sentiment'] >= 0.05:
-            cleaned_dict[str(max_lim)][8] += 1
+            s_dict[key][2] += 1
+        s_dict[key][0] /= nrows
+        s_dict[key][1] /= nrows
+        s_dict[key][2] /= nrows
             
-    return pandas.DataFrame.from_dict(cleaned_dict).transpose()
+    return (a_dict, b_dict, c_dict, s_dict)
 
-def visualize_over_time(df):
-    new_df = group_by_time_series(df)
-    new_df.set_axis(["A_Sentiment_Negative", "A_Sentiment_Neutral", "A_Sentiment_Positive", "B_Sentiment_Negative", "B_Sentiment_Neutral", "B_Sentiment_Positive", "C_Sentiment_Negative", "C_Sentiment_Neutral", "C_Sentiment_Positive"], axis=1,inplace=True)
+def visualize_over_time(df, name, nrows):
+    index = 0
+    for dict in group_by_time_series(df, nrows):
+        new_df = pandas.DataFrame.from_dict(dict).transpose()
+        new_df.columns = ["Negative", "Neutral", "Positive"]
+        new_df = new_df.rename_axis('Time_Num').reset_index()
 
-    print(df.head())
-    print(new_df.head())
-    #data_preproc = pandas.DataFrame({'Year': df['Time_Num'], 'A': df['A_Sentiment'], 'B': df['B_Sentiment'], 'C': df['C_Sentiment']})
-    #sns.lineplot(data=data_preproc[['A', 'B', 'C']])
-    #plt.show()
+        sns.lineplot(data=new_df[['Negative', 'Neutral', 'Positive']])
 
-def analyze_with_nssm(src):
+        sentiment_type = ""
+        if (index == 0):
+            sentiment_type = "NSSM_A"
+        elif (index == 1):
+            sentiment_type = "NSSM_B"
+        elif (index == 2):
+            sentiment_type = "NSSM_C"
+        elif (index == 3):
+            sentiment_type = "Vader"
+        
+        plt.xlabel = "Time"
+        plt.ylabel = "Sentiment (%)"
+        plt.title(name + ' Sentiment Analysis (' + sentiment_type + ')')
+        plt.show()
+        plt.savefig(sentiment_type + '_' + name + '.png')
+
+        index += 1
+  
+    
+
+def analyze_with_nssm(src, name):
     df = pandas.read_csv(src, encoding='utf-8', engine='python', names=['Date', 'Year', 'Month', 'Day', 'Author', 'Title', 'Body', 'Link', 'Section', 'Publication'], on_bad_lines='skip', nrows=1000)
 
     df['Sentiment'] = df['Body'].apply(sentiment)
     df = nssm.apply_nssm(df)
 
-    visualize_over_time(df)
+    visualize_over_time(df, name, len(df))
 
-analyze_with_nssm('./data/reuters.csv')
+analyze_with_nssm('./data/reuters.csv', 'Reuters')
+#analyze_with_nssm('./data/cnn.csv', 'CNN')
+#analyze_with_nssm('./data/fox.csv', 'Fox News')
+#analyze_with_nssm('./data/nytimes.csv', 'NY Times')
