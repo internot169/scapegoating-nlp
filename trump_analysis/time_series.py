7555bb0da5b0a065b03c094696775a5c897b2549
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-08-26 09:11:25 -0700

    Rearrange modules

--- trump_analysis/time_series.py
+++ trump_analysis/time_series.py
@@ -1,28 +1,13 @@
-from nltk import sent_tokenize
-import nssm
 import pandas
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 import seaborn as sns
+import vader_english
+import nssm_english
 
 # number of days to group for visualization
-GROUPING_SIZE = 30.4167
+GROUPING_SIZE = 10
 
-def sentiment(text):
-    vader = SentimentIntensityAnalyzer()
-
-    sentences = sent_tokenize(text)
-    sentences_count = len(sentences)
-    sentiment_sum = 0
-
-    for sentence in sentences:
-        sentiment_sum += vader.polarity_scores(sentence)['compound']
-
-    mean = sentiment_sum / sentences_count
-
-    return mean
-
-def group_by_time_series(df, nrows):
+def group_by_time_series(df):
     for index, row in df.iterrows():
         df.loc[index, 'Time_Num'] = (float(row['Year']) * 365) + (float(row['Month']) * 30.4167) + float(row['Day'])
     
@@ -44,7 +29,7 @@ def group_by_time_series(df, nrows):
     s_dict = {
 
     }
-
+    nrows = len(df)
     for index, row in df.iterrows():
         if row['Time_Num'] >= max_lim:
             max_lim += GROUPING_SIZE
@@ -59,9 +44,6 @@ def group_by_time_series(df, nrows):
             a_dict[key][1] += 1
         elif row['A_Sentiment_Final'] == 1:
             a_dict[key][2] += 1
-        a_dict[key][0] = (a_dict[key][0] / nrows) * 100
-        a_dict[key][1] = (a_dict[key][1] / nrows) * 100
-        a_dict[key][2] = (a_dict[key][2] / nrows) * 100
         
         if (key not in b_dict):
             b_dict[key] = [0,0,0]
@@ -71,9 +53,6 @@ def group_by_time_series(df, nrows):
             b_dict[key][1] += 1
         elif row['B_Sentiment_Final'] == 1:
             b_dict[key][2] += 1
-        b_dict[key][0] /= nrows
-        b_dict[key][1] /= nrows
-        b_dict[key][2] /= nrows
 
         if (key not in c_dict):
             c_dict[key] = [0,0,0]
@@ -83,9 +62,6 @@ def group_by_time_series(df, nrows):
             c_dict[key][1] += 1
         elif row['C_Sentiment_Final'] == 1:
             c_dict[key][2] += 1
-        c_dict[key][0] /= nrows
-        c_dict[key][1] /= nrows
-        c_dict[key][2] /= nrows
 
         if (key not in s_dict):
             s_dict[key] = [0,0,0]
@@ -95,18 +71,21 @@ def group_by_time_series(df, nrows):
             s_dict[key][1] += 1
         elif row['Sentiment'] >= 0.05:
             s_dict[key][2] += 1
-        s_dict[key][0] /= nrows
-        s_dict[key][1] /= nrows
-        s_dict[key][2] /= nrows
             
     return (a_dict, b_dict, c_dict, s_dict)
 
-def visualize_over_time(df, name, nrows):
+def visualize_over_time(df, name):
     index = 0
-    for dict in group_by_time_series(df, nrows):
+    for dict in group_by_time_series(df):
         new_df = pandas.DataFrame.from_dict(dict).transpose()
         new_df.columns = ["Negative", "Neutral", "Positive"]
         new_df = new_df.rename_axis('Time_Num').reset_index()
+
+        for i, row in new_df.iterrows():
+            total = row['Negative'] + row['Neutral'] + row['Positive']
+            new_df.loc[i, 'Negative'] = (new_df.loc[i, 'Negative'] / total) * 100
+            new_df.loc[i, 'Neutral'] = (new_df.loc[i, 'Neutral'] / total) * 100
+            new_df.loc[i, 'Positive'] = (new_df.loc[i, 'Positive'] / total) * 100
 
         sns.lineplot(data=new_df[['Negative', 'Neutral', 'Positive']])
 
@@ -131,14 +110,14 @@ def visualize_over_time(df, name, nrows):
     
 
 def analyze_with_nssm(src, name):
-    df = pandas.read_csv(src, encoding='utf-8', engine='python', names=['Date', 'Year', 'Month', 'Day', 'Author', 'Title', 'Body', 'Link', 'Section', 'Publication'], on_bad_lines='skip', nrows=1000)
+    df = pandas.read_csv(src, encoding='utf-8', engine='python', names=['Date', 'Year', 'Month', 'Day', 'Author', 'Title', 'Body', 'Link', 'Section', 'Publication'], on_bad_lines='skip')
 
-    df['Sentiment'] = df['Body'].apply(sentiment)
+    df['Sentiment'] = df['Body'].apply(vader.sentiment)
     df = nssm.apply_nssm(df)
 
-    visualize_over_time(df, name, len(df))
+    visualize_over_time(df, name)
 
-analyze_with_nssm('./data/reuters.csv', 'Reuters')
+#analyze_with_nssm('./data/reuters.csv', 'Reuters')
 #analyze_with_nssm('./data/cnn.csv', 'CNN')
 #analyze_with_nssm('./data/fox.csv', 'Fox News')
 #analyze_with_nssm('./data/nytimes.csv', 'NY Times')
---
+++ trump_analysis/vader_english.py
@@ -0,0 +1,19 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk import sent_tokenize
+
+# ALSO TRY WEIGHTED MEAN
+def sentiment(text):
+    vader = SentimentIntensityAnalyzer()
+    print(vader.summary())
+
+    sentences = sent_tokenize(text)
+    sentences_count = len(sentences)
+    sentiment_sum = 0
+
+    for sentence in sentences:
+        # Explain the compound normalization thing in the paper
+        sentiment_sum += vader.polarity_scores(sentence)['compound']
+
+    mean = sentiment_sum / sentences_count
+
+    return mean
